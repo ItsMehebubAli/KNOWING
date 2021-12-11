09f1bd160e6797a6a884dba5e49eaa02fa946ee3
itsMehebubAli <thecryptographer0.com>
2021-12-11 20:27:49 +0530

    first commit

---
+++ algo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+
+    printf("1.My name is Mehebub ali.\n");
+    printf("2.My hobbies are Reading books,Photography.\n");
+    printf("3.Currently i'am learning about C-language.\n");
+    printf("4.I did my high schooling from APS,Ranchi.\n");
+    printf("5.I'am currently reading 'The Psychology of Money'book.\n");
+
+    return 0;
+}
---
+++ operaters.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b,c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    scanf("%d",&d);
+    printf("a>b||c>d = %d\n",a>b||c>d);
+    return 0;
+}
