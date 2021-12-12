073e1bd756b97adefc6e90c6fa61ea62035202dd
itsMehebubAli <thecryptographer0.com>
2021-12-12 13:53:56 +0530

    first commit

---
+++ tables.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{   
+    int a;
+    printf("The multiplication table of the number\n");
+    scanf("%d",&a);
+    printf("%d",a);
+    printf("*1=%d\n",a*1);
+    printf("%d",a);  
+    printf("*2=%d\n",a*2);
+    printf("%d",a); 
+    printf("*3=%d\n",a*3);
+    printf("%d",a);
+    printf("*4=%d\n",a*4);
+    printf("%d",a); 
+    printf("*5=%d\n",a*5);
+    printf("%d",a); 
+    printf("*6=%d\n",a*6);
+    printf("%d",a); 
+    printf("*7=%d\n",a*7);
+    printf("%d",a);
+    printf("*8=%d\n",a*8);
+    printf("%d",a); 
+    printf("*9=%d\n",a*9);
+    printf("%d",a); 
+    printf("*10=%d\n",a*10);
+          
+    return 0;
+}
